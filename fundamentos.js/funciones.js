da42d4f2051cfecca8c38f630e8d0dc8e5838c25
chrisdw23 <christiandavidmendozamaximo@gmail.com>
2025-10-08 11:47:22 -0600

    first commit

---
+++ fundamentos.js/funciones.js
@@ -0,0 +1,38 @@
+// funciones
+
+function saluda(){
+    console.log('Hello')
+}
+saluda()
+function suma(a, b){
+    return a + b 
+}
+console.log(suma(4,5))
+
+const resta = function(a, b) {
+    return a - b
+}
+console.log(resta(10, 5))
+
+// Arrow fuctions
+const sayHello = () => {
+    console.log('Helloooo')
+}
+sayHello()
+
+const multiplica = (a, b) => a * b
+
+
+console.log(multiplica(10, 5))
+
+const duplica = n=> n * 2
+
+console.log(duplica(5))
+
+// Funciones de orden superior
+const ejecuta = (funcion, a, b) => {
+    return funcion(a,b)
+}
+console.log(ejecuta(suma, 20, 10))
+console.log(ejecuta(resta, 20, 10))
+console.log(ejecuta(multiplica, 20, 10))
---
+++ fundamentos.js/tipos.js
@@ -0,0 +1,50 @@
+    const pi = 3.14
+
+    let name = "Cristhian" // string
+    let lastNmae = 'Mendoza' // string
+
+    let isLate = true // boolean
+
+    let age // undefined
+    age = 20 // number
+
+    // Arrays
+    let numbers = [2, 5, 7, 9] // object - Array
+    console.log(numbers.length)
+
+    numbers.push(15) // inserta elemntos al final
+    numbers.pop() // extre elemnto final
+    numbers.shift() // extrae del inicio
+    numbers.unshift(19) // inserta al incio
+
+    numbers.push('otro')
+    console.log(numbers)
+
+    // mostrar string al reves 
+    let nombre = 'crsithian'
+    let nombreArray = nombre.split('')
+    console.log(nombreArray)
+    let array_reverse = nombreArray.reverse()
+    console.log(array_reverse)
+    let nombre_reverse = array reverse.join('')
+    console.log(nombre_reverse)
+
+    // Objetos
+    const group = {
+        grade: 4,
+        name: 'A',
+        career: 'TSU DMS',
+        students: ['Alan', 'Denisse', 'Emmanuel'],
+        profesor: 'Francisco'
+    }
+ 
+    console.log(group)
+    console.log(group.profesor)
+    console.log(group.students)
+    console.log(group.career)
+
+    group.tutor = 'Diana'
+    group.pet = 'Cat'
+
+    console.log(group)
+
